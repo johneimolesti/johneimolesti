d0a60684b45e2bb12a21a9b0f2a86f2300de464d
johneimolesti <johneimolesti@gmail.com>
2026-09-18 20:44:15 +0200

    Delete assets directory

--- assets/shared/supabase-config.js
+++
@@ -1,5 +0,0 @@
-window.JM_CONFIG = Object.freeze({
-  SUPABASE_URL: 'https://etzwybamvfpeitkttwrc.supabase.co',
-  SUPABASE_KEY: 'sb_publishable_CtyexwjoW375UXpjInOuDA_Uz28wWJx',
-  FAN_API: 'https://etzwybamvfpeitkttwrc.supabase.co/functions/v1/fan-api'
-});
